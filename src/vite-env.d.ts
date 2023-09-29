8dc1e5e33d3706394e129e3afeb61acb791eb6f0
samueljarry <samueljarry.sj@gmail.com>
2023-09-29 11:48:33 +0200

    Feat: Dynamic routing

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,29 @@
 /// <reference types="vite/client" />
+
+declare type Movie = {
+  title: string;
+  duration: number;
+  id: number;
+  releaseDate: string;
+  description: string;
+  actor: Actor[];
+  category: {
+    name: string;
+  };
+}
+
+declare type Actor = {
+  id: number;
+  lastName: string;
+  firstName: string;
+  movies: Movie[];
+  nationality: {
+    name: string;
+  }
+}
+
+declare type Category = {
+  id: number;
+  name: string;
+  movies: Movie[];
+}
